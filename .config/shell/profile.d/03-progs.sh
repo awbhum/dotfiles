1d9f9b6b1fee565a56089bbdb6822fdb41866ab5
awbhum <andywb707@gmail.com>
2023-11-27 13:23:33 -0800

    fix

--- .config/shell/profile.d/03-progs.sh
+++ .config/shell/profile.d/03-progs.sh
@@ -1,25 +1,30 @@
 #!/bin/sh
 
 # editor choice
-for i in nano vi vim.tiny vim.basic vim nvim; do
-    command -v $i >/dev/null 2>/dev/null && export EDITOR="$i"
+for i in nano vi vim.tiny vim.basic vim nvim
+    do command -v $i >/dev/null 2>&1 && export EDITOR="$i"
 done
 
 # use the editor as a visual editor
 export VISUAL="$EDITOR"
 
 # pager / manpager
-for i in more less; do
-    command -v $i >/dev/null 2>/dev/null && export PAGER="$i"
+for i in more less
+    do command -v $i >/dev/null 2>&1 && export PAGER="$i"
 done
 
 # render manpages using the normal pager
 export MANPAGER="$PAGER"
 
 # video player
-for i in mplayer smplayer vlc mpv celluloid; do
-    command -v $i >/dev/null 2>/dev/null && export VPLAYER="$i"
+for i in mplayer smplayer vlc mpv celluloid
+    do command -v $i >/dev/null 2>&1 && export VPLAYER="$i"
 done
 
 # open audio files in the video player
 export APLAYER="$VPLAYER"
+
+# open files using open-file
+for i in xdg-open open-file
+    do command -v $i >/dev/null 2>&1 && export OPENER="$i"
+done
