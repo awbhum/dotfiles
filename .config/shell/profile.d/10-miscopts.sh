1626afe9cfc7a053f466584ce9f6c9f0798d4bb2
awbhum <andywb707@gmail.com>
2023-11-26 20:36:29 -0800

    test

--- .config/shell/profile.d/10-miscopts.sh
+++ .config/shell/profile.d/10-miscopts.sh
@@ -13,17 +13,14 @@ export CURL_HOME="${XDG_CONFIG_HOME:-$HOME/.config}/curl"
 export GIMP2_DIRECTORY="${XDG_CONFIG_HOME:-$HOME/.config}/gimp"
 export NOTMUCH_CONFIG="${XDG_CONFIG_HOME:-$HOME/.config}/notmuch/config"
 
-# x11 stuff
-[ "$XDG_SESSION_TYPE" = "x11" ] && {
-    # xinitrc in .config
-    export XINITRC="${XDG_CONFIG_HOME:-$HOME/.config}/x11/xinitrc"
+# xinitrc in .config
+export XINITRC="${XDG_CONFIG_HOME:-$HOME/.config}/x11/xinitrc"
 
-    # doesn't work with all display managers/desktops
-    case "$WM_USE" in
-        '') export XAUTHORITY="$HOME/.Xauthority" ;;
-        *) export XAUTHORITY="${XDG_DATA_HOME:-$HOME/.local/share}/xauthority" ;;
-    esac
-}
+# move xauthority out of ~, doesn't work with all display managers/desktops
+case "$WM_USE" in
+    '(placeholder)') export XAUTHORITY="$HOME/.Xauthority" ;;
+    *) export XAUTHORITY="${XDG_DATA_HOME:-$HOME/.local/share}/xauthority" ;;
+esac
 
 # move private things into ~/.local/share
 export GNUPGHOME="${XDG_DATA_HOME:-$HOME/.local/share}/gnupg"
@@ -33,14 +30,16 @@ export PASSWORD_STORE_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/passwords"
 export TMUX_TMPDIR="${XDG_RUNTIME_DIR:-/run/user/${EUID:-${UID:-$(id -u)}}}"
 
 # 'watch' update interval
-export WATCH_INTERVAL="1"
+export WATCH_INTERVAL="0.5"
 
 # gpg terminal
 [ -n "$(command -v tty)" ] && export GPG_TTY="$(tty)"
 
 # set the GPG signing key
-IFS="asodasidhuasiduh" read -r GPG_SIGNING_KEY <"${GNUPGHOME:-$HOME/.local/share/gnupg}/key"
-export GPG_SIGNING_KEY="$GPG_SIGNING_KEY"
+[ -r "${GNUPGHOME:-$HOME/.local/share/gnupg}/key" ] && {
+    IFS="asodasidhuasiduh" read -r GPG_SIGNING_KEY <"${GNUPGHOME:-$HOME/.local/share/gnupg}/key"
+    export GPG_SIGNING_KEY="$GPG_SIGNING_KEY"
+}
 
 # other misc. environment variables
 export MOZ_USE_XINPUT2="1"
