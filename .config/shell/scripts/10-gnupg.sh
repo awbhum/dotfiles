13f1a7230fd2d07dc1977b70ec7b5bffa47465a3
awbhum <andywb707@gmail.com>
2023-12-05 22:22:59 -0800

    gpg update

--- .config/shell/scripts/10-gnupg.sh
+++ .config/shell/scripts/10-gnupg.sh
@@ -7,10 +7,23 @@ export GNUPGHOME="${XDG_DATA_HOME:-$HOME/.local/share}/gnupg"
 export PASSWORD_STORE_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/password-store"
 
 # gpg terminal
-[ -n "$(command -v tty)" ] && export GPG_TTY="$(tty)"
+command -v tty >/dev/null 2>&1 && export GPG_TTY="$(tty)"
+
+# create the gnupg home dir
+[ -d "${GNUPGHOME:-${XDG_DATA_HOME:-$HOME/.local/share}/gnupg}" ] || mkdir -pv "${GNUPGHOME:-${XDG_DATA_HOME:-$HOME/.local/share}/gnupg}"
+
+# change permissions for gnupg home if necessary
+command -v stat chmod >/dev/null 2>&1 && {
+    case "$(stat -c '%a' ${GNUPGHOME:-${XDG_DATA_HOME:-$HOME/.local/share}/gnupg})" in
+        *00) ;;
+        *) chmod -v 700 ${GNUPGHOME:-${XDG_DATA_HOME:-$HOME/.local/share}/gnupg} ;;
+    esac
+}
 
 # set the GPG signing key
 [ -r "${GNUPGHOME:-$HOME/.local/share/gnupg}/key" ] && {
-    IFS="$(head -c4 </dev/urandom)" read -r GPG_SIGNING_KEY <"${GNUPGHOME:-$HOME/.local/share/gnupg}/key"
-    export GPG_SIGNING_KEY="$GPG_SIGNING_KEY"
+    command -v head >/dev/null 2>&1 && {
+        IFS="$(head -c4 </dev/urandom)" read -r GPG_SIGNING_KEY <"${GNUPGHOME:-$HOME/.local/share/gnupg}/key"
+        export GPG_SIGNING_KEY="$GPG_SIGNING_KEY"
+    }
 }
