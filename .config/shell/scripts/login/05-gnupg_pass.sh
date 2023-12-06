5fed6b3b810f1907a0f5add49485844d9c321d2e
awbhum <andywb707@gmail.com>
2023-12-06 13:02:50 -0800

    a

--- .config/shell/scripts/login/05-gnupg_pass.sh
+++ .config/shell/scripts/login/05-gnupg_pass.sh
@@ -9,4 +9,11 @@ esac
 # check if gpg already exists
 [ -r "${GNUPGHOME:-$HOME/.gnupg}/pubring.kbx" ] || {
     printf "\n\033[1mNo GnuPG keys found. Run \`\033[22mgpg --full-gen-key\033[1m\` or set DISABLE_GNUPG=y to disable this message.\033[22m\n"
+    return 1
+}
+
+# check if password store already exists
+[ -r "${PASSWORD_STORE_DIR:-$HOME/.password-store}/.gpg-id" ] || {
+    printf "\n\033[1mNo password store found. Run \`\033[22mpass init [GPG_ID]\033[1m\` or set DISABLE_GNUPG=y to disable this message.\033[22m\n"
+    return 1
 }
