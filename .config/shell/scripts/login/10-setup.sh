a71ea1472ed65f4748f5da766cdaf521755ce9eb
awbhum <andywb707@gmail.com>
2023-12-06 09:55:49 -0800

    newline

--- .config/shell/scripts/login/10-setup.sh
+++ .config/shell/scripts/login/10-setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+printf "\n"
+
 # run the deploy script if required
 [ -r "${XDG_DATA_HOME:-$HOME/.local/share}/dots/.configured.txt" ] || {
     command -v deploy-dots >/dev/null 2>&1 && deploy-dots
