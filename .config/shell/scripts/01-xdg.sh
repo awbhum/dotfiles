eae61ea8fadc3e93facbb4e3d9b5d60bb6c6ced6
awbhum <andywb707@gmail.com>
2023-12-10 00:45:32 -0800

    a

--- .config/shell/scripts/01-xdg.sh
+++ .config/shell/scripts/01-xdg.sh
@@ -4,7 +4,7 @@
 export XDG_TEMPLATES_DIR="${XDG_CACHE_HOME:-$HOME/.cache/template}"
 export XDG_PUBLICSHARE_DIR="$HOME/public"
 export XDG_DESKTOP_DIR="$HOME/desktop"
-export XDG_DOCUMENTS_DIR="$HOME/docs"
+export XDG_DOCUMENTS_DIR="$HOME/."
 export XDG_DOWNLOAD_DIR="$HOME/."
 export XDG_MUSIC_DIR="$HOME/music"
 export XDG_PICTURES_DIR="$HOME/pics"
