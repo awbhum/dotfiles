55197711c367f71ede3e4b6e5a87ad63a686824b
awbhum <andywb707@gmail.com>
2023-12-10 18:51:34 -0800

    more theme engine integration

---
+++ .config/river/___theme.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# theme template for riverwm
+
+# set background and border color
+riverctl background-color 0x130e0e
+riverctl border-color-focused 0x64494a
+riverctl border-color-unfocused 0x322425
