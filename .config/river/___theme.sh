b117c18f5a5ad966909cb9b0d2269865d976a28a
awbhum <andywb707@gmail.com>
2023-12-11 13:05:14 -0800

    background colors are generated

--- .config/river/___theme.sh
+++ .config/river/___theme.sh
@@ -3,6 +3,6 @@
 # theme template for riverwm
 
 # set background and border color
-riverctl background-color 0x130e0e
-riverctl border-color-focused 0x64494a
-riverctl border-color-unfocused 0x322425
+riverctl background-color 0x#::background0::#
+riverctl border-color-focused 0x#::background11::#
+riverctl border-color-unfocused 0x#::background5::#
