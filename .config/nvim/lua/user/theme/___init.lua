aa8fbb1530c6ffb9d12e7ff1175c8eee5910381a
awbhum <andywb707@gmail.com>
2023-12-18 14:45:33 -0800

    a

--- .config/nvim/lua/user/theme/___init.lua
+++ .config/nvim/lua/user/theme/___init.lua
@@ -4,4 +4,10 @@ ThemeIntegrations = {
 }
 
 -- set the colorscheme
-vim.cmd("silent! colorscheme #::vim-colorscheme::#")
+vim.cmd([[
+    try
+        silent! colorscheme #::vim-colorscheme::#
+    catch /^Vim\%((\a\+)\)\=:E185/
+        silent! colorscheme fallback
+    endtry
+]])
