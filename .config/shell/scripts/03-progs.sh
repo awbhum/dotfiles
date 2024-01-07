8ad745f8e330c8ae2009e97e54df4a2087e96088
awbhum <andywb707@gmail.com>
2024-01-07 04:08:01 -0800

    changington

--- .config/shell/scripts/03-progs.sh
+++ .config/shell/scripts/03-progs.sh
@@ -13,17 +13,17 @@ for i in nano micro vi nvi vim.tiny vim.basic vim nvim
     do command -v $i >/dev/null 2>&1 && export EDITOR="$i"
 done
 
+# whether the editor has a GUI or operates in the terminal
+export EDITOR_GUI="y"
 
 # use the editor as a visual editor
 export VISUAL="$EDITOR"
-
 
 # pager / manpager
 for i in more less
     do command -v $i >/dev/null 2>&1 && export PAGER="$i"
 done
 export MANPAGER="$PAGER"
-
 
 # browser
 for i in amfora w3m links lynx elinks links2
@@ -35,7 +35,6 @@ done
     done
 }
 
-
 # video player/image viewer
 for i in timg mpv
     do command -v $i >/dev/null 2>&1 && export VPLAYER="xdg-terminal-exec $i"
@@ -46,17 +45,15 @@ done
     done
 }
 
-
 # image viewer
 for i in catimg timg fbida mpv fbpdf fbv tiv viu
     do command -v $i >/dev/null 2>&1 && export IMGVIEWER="xdg-terminal-exec $i"
 done
 [ -n "${WAYLAND_DISPLAY:-$DISPLAY}" ] && {
-    for i in deepin-image-viewer aloadimage gthumb feh ahoviewer eom geeqie gpicview mirage lximage-qt viewnior phototonic swayimg gwenview nomacs qview qimgv feh celluloid mpv qiv vimiv pqiv imv sxiv nsxiv
+    for i in deepin-image-viewer aloadimage gthumb ahoviewer eom geeqie gpicview mirage lximage-qt viewnior phototonic gwenview celluloid mpv swayimg nomacs qview qimgv feh qiv vimiv pqiv imv sxiv nsxiv
         do command -v $i >/dev/null 2>&1 && export IMGVIEWER="$i"
     done
 }
-
 
 # pdf reader
 for i in lesspdf fbv fbpdf
@@ -68,13 +65,12 @@ done
     done
 }
 
-
-# terminal file manager preferred
-[ -n "${WAYLAND_DISPLAY:-$DISPLAY}" ] && {
+# terminal file manager preferred in simple environments
+for i in fff shfm ranger cfm clifm mc vifm lfm nnn lf
+    do command -v $i >/dev/null 2>&1 && export FILEMGR="xdg-terminal-exec $i"
+done
+[ -n "${WAYLAND_DISPLAY:-$DISPLAY}" ] && [ -n "$XDG_CURRENT_DESKTOP" ] && {
     for i in nautilus caja rox xfe pcmanfm thunar nemo krusader dolphin qtfm
         do command -v $i >/dev/null 2>&1 && export FILEMGR="$i"
     done
 }
-for i in fff shfm ranger cfm clifm mc vifm lfm nnn lf
-    do command -v $i >/dev/null 2>&1 && export FILEMGR="xdg-terminal-exec $i"
-done
